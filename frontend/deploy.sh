978ba3a1854d01b26cba65b73f242053c3b4e1d2
miles <miles@milesdeMac-mini.local>
2025-08-16 02:36:58 +0800

    配置自定义域名支持

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -7,16 +7,16 @@ pnpm run build
 # 进入生成的文件夹
 cd dist
 
-# 如果是发布到自定义域名（如果你有域名的话）
-# echo 'simple-maker.fun' > CNAME
+# 如果是发布到自定义域名（请替换为你的域名）
+echo 'simple-maker.fun' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 发布到 GitHub Pages (使用HTTPS)
-# 请将下面的 USERNAME 和 REPO 替换为你的 GitHub 用户名和仓库名
-git push -f https://github.com/Miles2048/tier-ranking.git main:gh-pages
+# 请将下面的仓库名替换为新的仓库名
+git push -f https://github.com/Miles2048/simple-maker.git main:gh-pages
 
 
 cd -
